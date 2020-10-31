c7779175fea52e0f454276c85ff16df7e9e1639e
Michael Mason <TeaUponTweed@gmail.com>
2020-10-31 12:02:30 -0600

    handle rpi in scraping

--- scrape_image.sh
+++ scrape_image.sh
@@ -1,1 +1,5 @@
-"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"  --headless --screenshot --window-size=900,1000 --default-background-color=0  https://www.google.com/search?q=weather
+if [ "$(uname)" == "Darwin" ]; then
+	"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"  --headless --screenshot --window-size=900,1000 --default-background-color=0  https://www.google.com/search?q=weather
+else
+	chromium-browser --headless --screenshot --window-size=900,1000 --default-background-color=0  https://www.google.com/search?q=weather
+fi

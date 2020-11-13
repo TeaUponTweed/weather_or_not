e74cd7f6482b0c0d681c48412a23f939b07215d8
Michael Mason <TeaUponTweed@gmail.com>
2020-11-13 08:53:36 -0700

    get scrape_image.sh to be somewhat path independent

--- scrape_image.sh
+++ scrape_image.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
+set -e
+
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd $SCRIPTPATH
+
 if [ "$(uname)" == "Darwin" ]; then
 	"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"  --headless --screenshot --window-size=900,1000 --default-background-color=0  https://www.google.com/search?q=weather
 else
 	chromium-browser --headless --screenshot --window-size=900,1000 --default-background-color=0  https://www.google.com/search?q=weather
 fi
-python3 weather_display.py make_weather_bw screenshot.png template.txt --inky
+
+python3 ./weather_display.py make_weather_bw screenshot.png template.txt --inky
+
+cd -

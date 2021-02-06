0086ccc3082a48498289836c0177a85d408c75ae
Michael Mason <TeaUponTweed@gmail.com>
2021-02-06 14:48:22 -0700

    update update_display.sh

--- update_display.sh
+++ update_display.sh
@@ -2,13 +2,17 @@
 
 set -e
 
+export GEONAMES_USER=$SET_ME
+export OPENWEATHER_TOKEN=$SET_ME
+
+
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPTPATH
 
 sleep 3
 python3 image_from_api.py main '"Parker, CO"' latest.png
 sleep 3
-python3 ./weather_display.py inky_show latest.png
+python3 ./inky_show.py latest.png
 
 
 cd -
